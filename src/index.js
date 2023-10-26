77f96df8a6d20525d74e100136e2496e0fa49ceb
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-27 00:23:03 +0530

    commitsec

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Fragment } from "react";
+import ReactDOM  from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Fragment>
+   <div className="cards">
+   <div className="card">
+   <img src="https://wallpaperaccess.com/full/1092351.jpg" alt="my_pic" className="card_img" />
+   <span className="card_catergory">A Netflix Original Series</span>
+   <h3 className="card_title">LUSIFER 2020</h3>
+   <a href="https://www.netflix.com/in/title/80057918"target="_blanck">
+   <button>Watch Now</button>
+    </a>
+   </div>
+   </div>
+  </Fragment>
+  ,document.getElementById('root'));
