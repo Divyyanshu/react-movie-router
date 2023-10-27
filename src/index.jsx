eb715df8e12241aa6e1c2f3b85ad1657e7138675
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-27 21:20:18 +0530

    complete

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import { Fragment } from "react";
 import Card from "./Card";
 import './index.css';
-
+import Heading from "./Heading";
 
 ReactDOM.render(
   <Fragment>
+  <Heading/>
     <Card
       imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
       title="Netflix Original"
@@ -35,70 +36,21 @@ ReactDOM.render(
       sname="Titanic"
       link="https://www.netflix.com/in/title/80100172" />
       <Card
-      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      imgsrc="https://wallpapercave.com/dwp1x/wp12445684.jpg"
       title="Netflix Original"
-      sname="Dark"
+      sname="Bloddy Daddy"
       link="https://www.netflix.com/in/title/80100172"
     />
     <Card
-      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
+      imgsrc="https://wallpapercave.com/dwp1x/wp8637980.jpg"
       title="Netflix Original"
-      sname="Stranger Things "
+      sname="Money Heist "
       link="https://www.netflix.com/in/title/80100172" />
 
-    <Card
-      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
-      title="Netflix Original"
-      sname="Harry Potter"
-      link="https://www.netflix.com/in/title/80100172" />
-
-    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
-      title="Netflix Original"
-      sname="Life OF Pie"
-      link="https://www.netflix.com/in/title/80100172" />
       <Card
-      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      imgsrc="https://wallpapercave.com/wp/wp8872294.jpg"
       title="Netflix Original"
-      sname="Dark"
-      link="https://www.netflix.com/in/title/80100172"
-    />
-    <Card
-      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
-      title="Netflix Original"
-      sname="Stranger Things "
-      link="https://www.netflix.com/in/title/80100172" />
-
-    <Card
-      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
-      title="Netflix Original"
-      sname="Harry Potter"
-      link="https://www.netflix.com/in/title/80100172" />
-
-    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
-      title="Netflix Original"
-      sname="Life OF Pie"
-      link="https://www.netflix.com/in/title/80100172" />
-      <Card
-      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
-      title="Netflix Original"
-      sname="Dark"
-      link="https://www.netflix.com/in/title/80100172"
-    />
-    <Card
-      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
-      title="Netflix Original"
-      sname="Stranger Things "
-      link="https://www.netflix.com/in/title/80100172" />
-
-    <Card
-      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
-      title="Netflix Original"
-      sname="Harry Potter"
-      link="https://www.netflix.com/in/title/80100172" />
-
-    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
-      title="Netflix Original"
-      sname="Life OF Pie"
+      sname="La La Land"
       link="https://www.netflix.com/in/title/80100172" />
   </Fragment>,
 
