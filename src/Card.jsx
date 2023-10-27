a4a58227918984941b771849f7400901d2ca84ab
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-27 18:59:18 +0530

    com[plete ui = functioning

---
+++ src/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Fragment } from "react";
+import "./index.css";
+
+function Card(props) {
+  console.log(props);
+  return (
+    <Fragment>
+      <div className="cards">
+        <div className="card">
+          <img src={props.imgsrc} alt="" className="card_image" />
+          <div className="card_info">
+            <p className="card_category">{props.title}</p>
+            <h3 className="card_name">{props.sname}</h3>
+            <a href={props.link} target="_blan">
+              <button>Watch Movie</button>{" "}
+            </a>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Card;
---
+++ src/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+import Card from "./Card";
+import './index.css';
+
+
+ReactDOM.render(
+  <Fragment>
+    <Card
+      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      title="Netflix Original"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
+      title="Netflix Original"
+      sname="Stranger Things "
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
+      title="Netflix Original"
+      sname="Harry Potter"
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
+      title="Netflix Original"
+      sname="Life OF Pie"
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp11941863.jpg"
+      title="Netflix Original"
+      sname="Titanic"
+      link="https://www.netflix.com/in/title/80100172" />
+      <Card
+      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      title="Netflix Original"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
+      title="Netflix Original"
+      sname="Stranger Things "
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
+      title="Netflix Original"
+      sname="Harry Potter"
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
+      title="Netflix Original"
+      sname="Life OF Pie"
+      link="https://www.netflix.com/in/title/80100172" />
+      <Card
+      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      title="Netflix Original"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
+      title="Netflix Original"
+      sname="Stranger Things "
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
+      title="Netflix Original"
+      sname="Harry Potter"
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
+      title="Netflix Original"
+      sname="Life OF Pie"
+      link="https://www.netflix.com/in/title/80100172" />
+      <Card
+      imgsrc="https://wallpaperaccess.com/full/1605503.jpg"
+      title="Netflix Original"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp3687626.jpeg"
+      title="Netflix Original"
+      sname="Stranger Things "
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card
+      imgsrc="https://wallpapercave.com/dwp1x/wp6063868.jpg"
+      title="Netflix Original"
+      sname="Harry Potter"
+      link="https://www.netflix.com/in/title/80100172" />
+
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp1864240.jpg"
+      title="Netflix Original"
+      sname="Life OF Pie"
+      link="https://www.netflix.com/in/title/80100172" />
+  </Fragment>,
+
+  document.getElementById("root")
+);
